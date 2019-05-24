7aba83bac01084d14d6058f7cbea0eb1af861cc3
kunal <kunalbhatia2409@gmail.com>
2019-05-24 17:32:53 +0530

    Updates

---
+++ precache-manifest.4743bdad41ee35afe3e7ac3632be4492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5332cc8456882558e403d3c13e139445",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "23225da29a60fc471c96",
+    "url": "/snake-game/static/css/main.d2909d16.chunk.css"
+  },
+  {
+    "revision": "becf07cbf76db766d64c",
+    "url": "/snake-game/static/js/2.a9297b21.chunk.js"
+  },
+  {
+    "revision": "23225da29a60fc471c96",
+    "url": "/snake-game/static/js/main.226ff54b.chunk.js"
+  },
+  {
+    "revision": "9a755d0ffde03a58c68a",
+    "url": "/snake-game/static/js/runtime~main.356adb96.js"
+  }
+]);
